17a2cf0edf05728b69d9abcdafa3b7bac03dd93a
Coromoto <cleon@ull.edu.es>
2020-11-11 06:40:57 +0000

    Estado del repositorio al ejecutar $bundle gem gema
     En la rama master
    
     Confirmación inicial
    
     Cambios a ser confirmados:
    	nuevo archivo:  .gitignore
    	nuevo archivo:  .rspec
    	nuevo archivo:  .travis.yml
    	nuevo archivo:  Gemfile
    	nuevo archivo:  README.md
    	nuevo archivo:  Rakefile
    	nuevo archivo:  bin/console
    	nuevo archivo:  bin/setup
    	nuevo archivo:  gema.gemspec
    	nuevo archivo:  lib/gema.rb
    	nuevo archivo:  lib/gema/version.rb
    	nuevo archivo:  spec/gema_spec.rb
    	nuevo archivo:  spec/spec_helper.rb

---
+++ lib/gema.rb
@@ -0,0 +1,6 @@
+require "gema/version"
+
+module Gema
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/gema_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Gema do
+  it "has a version number" do
+    expect(Gema::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
