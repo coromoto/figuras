1954956e1afd293dc1f5425e4a9c8e03f210d91b
Coromoto <cleon@ull.edu.es>
2020-11-17 18:26:58 +0000

    Coordenada x de la clase Punto

--- lib/gema/punto.rb
+++ lib/gema/punto.rb
@@ -1,4 +1,5 @@
 class Punto
+	attr_reader :x
 	def initialize(x,y)
 		@x,@y = x, y
 	end
--- spec/punto_spec.rb
+++ spec/punto_spec.rb
@@ -1,7 +1,13 @@
 RSpec.describe Gema do
   describe Punto do    # Agrupa pruebas
+	 before :each do
+		 @origen = Punto.new(0,0)
+	 end
 	 it "Se debe poder instanciar un punto" do  # Describe una prueba 
-		 expect(Punto.new(0,0)).not_to eq(nil)
+		 expect(@origen).not_to eq(nil)
+	 end
+	 it "Se accede correctamente a la coordenada x" do  # Describe una prueba 
+		 expect(@origen.x).to eq(0)
 	 end
   end
 end
